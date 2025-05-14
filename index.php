5f0361aa29e4501f396d15c7d2c5ffa7c205782b
Edilan Pereira Barbosa <edilanpereira1511@gmail.com>
2025-05-14 16:39:47 -0300

    at

--- index.php
+++ index.php
@@ -9,38 +9,8 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg ">
-  <div class="container-fluid">
-  <a class="navbar-brand" href="#">E.B</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <div class="collapse navbar-collapse justify-content-end">
-    <ul class="navbar-nav">
-      <li class="nav-item active mx-3">
-        <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
-      </li>
-      <li class="nav-item mx-3">
-        <a class="nav-link" href="#">Sobre</a>
-      </li>
-      <li class="nav-item mx-3">
-        <a class="nav-link" href="#">Skills</a>
-      </li>
-      <li class="nav-item mx-3">
-        <a class="nav-link" href="#">Portfólio</a>
-      </li>
-       <li class="nav-item mx-3">
-        <a class="nav-link" href="#">Contato</a>
-      </li>
-    </ul>
-  </div>
-  </div>
-  </div>
-</nav>
-
-
+  <div class="container">
   <div class="row">
   <div class="col-sm-12">
 
@@ -72,6 +42,7 @@
   <footer>
     <p>&copy; 2025 Edilan Pereira</p>
   </footer>
+</div>
 </div>
 </div>
 
